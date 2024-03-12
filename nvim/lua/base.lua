d574c39ccdbcc26a74c0e99f2740c7544b13bd9a
Koya Masuda <masuda.58.koya@gmail.com>
2024-03-12 10:43:19 +0900

    wsl環境ではなくなったので、yankとクリップボード周りの記述を削除

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -1,6 +1,5 @@
 -- leader
 vim.g.mapleader = " "
-vim.opt.termguicolors = true
 
 -- encoding
 vim.opt.encoding = 'utf-8'
@@ -43,17 +42,4 @@ vim.keymap.set('n', 's', '"_s')
 
 -- clipboard
 vim.opt.clipboard = 'unnamedplus'
-if vim.fn.has("wsl") then
-  vim.g.clipboard = {
-    name = "win32yank-wsl",
-    copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf"
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --crlf",
-      ["*"] = "win32yank.exe -o --crlf"
-    },
-    cache_enable = 0,
-  }
-end
+
